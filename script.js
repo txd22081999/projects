19e1b8c0ed85fe7210a0abaeca553c4d6d3aa085
txd22081999 <txd22081999@gmail.com>
2021-01-26 00:45:10 +0700

    update

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
 window.onload = () => {
-  const card = document.querySelector("#card1");
-  const video = document.querySelector("#vid1");
-  const text = card.querySelector(".text");
-  const videoContainer = card.querySelector(".video-container");
+  const card = document.querySelector('#card1');
+  const content = card.querySelector('.content');
+  const video = document.querySelector('#vid1');
+  const text = card.querySelector('.text');
+  const videoContainer = card.querySelector('.video-container');
 
-  console.log(videoContainer);
+  // console.log(videoContainer);
 
-  card.addEventListener("mouseenter", () => {
+  card.addEventListener('click', () => {
+    console.log('CLICK');
+    window.open('https://google.com');
+  });
+
+  const copyEmail = card.querySelector('i.email');
+  const email = card.querySelector('span.email').innerText;
+  const copyPassword = card.querySelector('i.password');
+  const password = card.querySelector('span.password').innerText;
+
+  copyEmail.addEventListener('click', (e) => {
+    e.stopPropagation();
+    navigator.clipboard.writeText(email);
+  });
+
+  copyPassword.addEventListener('click', (e) => {
+    e.stopPropagation();
+    navigator.clipboard.writeText(password);
+  });
+
+  card.addEventListener('mouseenter', () => {
     setTimeout(() => {
-      text.classList.add("blur");
-
-      //   video.style.visibility = "visible";
-      //   video.currentTime = 0;
-      //   video.play();
-
-      //   videoContainer.classList.add("black");
-
-      console.log(video);
+      // text.classList.add('blur');
     }, 1000);
   });
 
-  card.addEventListener("mouseleave", () => {
-    videoContainer.classList.remove("black");
-    text.classList.remove("blur");
-    video.pause();
-    video.style.visibility = "hidden";
+  card.addEventListener('mouseleave', () => {
+    // text.classList.remove('blur');
   });
 };
