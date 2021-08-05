0216d9cf4156c171bafa7dd1bd3cdd6b7edcdc1b
Duy Tran <txd22081999@gmail.com>
2021-08-06 02:17:27 +0700

    update c-store link

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 const links = [
-  "https://christmas-landing-page-2208.netlify.app/",
-  "https://linkedin-clone-2208.web.app/",
-  "https://landing-ui-2208.netlify.app/",
-  "https://c-store-2208.netlify.app",
-  "https://slack-clone-56d3a.web.app/",
-  "https://login-form-iota.vercel.app/",
-  "https://mask-ui-2208.netlify.app/",
-  "https://card-ui-01-2208.netlify.app/",
-  "https://input-field-ui-2208.netlify.app/",
-  "https://card-smoke-2208.netlify.app",
-  "https://world-ranks-2208.vercel.app/",
-  "https://icon-hover-2208.netlify.app/",
-];
+  'https://christmas-landing-page-2208.netlify.app/',
+  'https://linkedin-clone-2208.web.app/',
+  'https://landing-ui-2208.netlify.app/',
+  'https://c-store-8022.netlify.app/',
+  'https://slack-clone-56d3a.web.app/',
+  'https://login-form-iota.vercel.app/',
+  'https://mask-ui-2208.netlify.app/',
+  'https://card-ui-01-2208.netlify.app/',
+  'https://input-field-ui-2208.netlify.app/',
+  'https://card-smoke-2208.netlify.app',
+  'https://world-ranks-2208.vercel.app/',
+  'https://icon-hover-2208.netlify.app/',
+]
 
 window.onload = () => {
-  console.log("load script");
-  const cards = document.querySelectorAll(".card");
+  console.log('load script')
+  const cards = document.querySelectorAll('.card')
   cards.forEach((card) => {
-    const index = card.getAttribute("data-index");
+    const index = card.getAttribute('data-index')
     if (index) {
-      card.addEventListener("click", () => {
-        window.open(links[index]);
-      });
+      card.addEventListener('click', () => {
+        window.open(links[index])
+      })
     }
-    handleCard(card);
-  });
+    handleCard(card)
+  })
   // console.log(index);
   // console.log(cards);
-};
+}
 
 const handleCard = (card) => {
   // const card = document.querySelector("#card1");
@@ -37,21 +37,21 @@ const handleCard = (card) => {
   //   window.open("https://google.com");
   // });
 
-  const copyEmail = card.querySelector("i.email");
+  const copyEmail = card.querySelector('i.email')
   if (!copyEmail) {
-    return;
+    return
   }
-  const email = card.querySelector("span.email").innerText;
-  const copyPassword = card.querySelector("i.password");
-  const password = card.querySelector("span.password").innerText;
+  const email = card.querySelector('span.email').innerText
+  const copyPassword = card.querySelector('i.password')
+  const password = card.querySelector('span.password').innerText
 
-  copyEmail.addEventListener("click", (e) => {
-    e.stopPropagation();
-    navigator.clipboard.writeText(email);
-  });
+  copyEmail.addEventListener('click', (e) => {
+    e.stopPropagation()
+    navigator.clipboard.writeText(email)
+  })
 
-  copyPassword.addEventListener("click", (e) => {
-    e.stopPropagation();
-    navigator.clipboard.writeText(password);
-  });
-};
+  copyPassword.addEventListener('click', (e) => {
+    e.stopPropagation()
+    navigator.clipboard.writeText(password)
+  })
+}
